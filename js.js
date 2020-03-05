6ed2c143213504c620cd1622da137923bd07fc35
guiferrini <guilhermemferrini@gmail.com>
2020-03-05 21:12:54 +0100

    Add wine page

--- js.js
+++ js.js
@@ -1,2 +1,14 @@
--pront se eh maior de idade tem 18, sim EventTarget, nao fecha
--clica e te mostar ficha tecnica do vinho
+//-pront se eh maior de idade tem 18, sim EventTarget, nao fecha
+//-clica e te mostar ficha tecnica do vinho
+function vinho1 () {
+    let ola = "nome: haha"
+    document.getElementById("resp").innerHTML = ola;
+}
+
+function vinho2 () {
+    document.getElementById("resp").innerHTML = "haha";
+}
+
+function vinho3 () {
+    document.getElementById("resp").innerHTML = "kkk";
+}
